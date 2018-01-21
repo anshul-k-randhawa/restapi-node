3fa19250effc36046ccec481765c6bba557673d3
Anshul Kumar <anshul.k.randhawa@gmail.com>
2018-01-22 00:51:24 +0530

    second

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ app.use(function(req, res) {
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('todo list RESTful node API server started on: ' + port);
